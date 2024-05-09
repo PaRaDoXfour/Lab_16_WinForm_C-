65a6801026d0414023c19bfe0f507eac2ec98378
Kostian Oleg <159447875+PaRaDoXfour@users.noreply.github.com>
2024-05-09 16:30:36 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ЛР_16_Editing_pictures_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSave.Location = new System.Drawing.Point(0, 720);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(1195, 27);
+            this.buttonSave.TabIndex = 0;
+            this.buttonSave.Text = "Зберегти";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonClear.Location = new System.Drawing.Point(0, 693);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(1195, 27);
+            this.buttonClear.TabIndex = 1;
+            this.buttonClear.Text = "Очистити";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonOpen.Location = new System.Drawing.Point(0, 666);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(1195, 27);
+            this.buttonOpen.TabIndex = 2;
+            this.buttonOpen.Text = "Огляд";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(1195, 666);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox.TabIndex = 3;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseDown);
+            this.pictureBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseMove);
+            this.pictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1195, 747);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonSave);
+            this.Name = "Form1";
+            this.Text = "Редагування малюнка";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+using System.Text;
+using System.Windows.Forms;
+
+namespace ЛР_16_Editing_pictures_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Bitmap bmp_for_draw;
+        Point start_point;
+        bool dozvil;
+        Pen pen1 = new Pen(Color.Black, 5);
+
+        string full_name_of_image;
+
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                using (Graphics g = Graphics.FromImage(pictureBox.Image))
+                {
+                    if (dozvil == true)
+                    {
+                        g.DrawLine(pen1, start_point, e.Location);
+                        start_point = e.Location;
+                        pictureBox.Invalidate();
+                    }
+                }
+            }
+        }
+
+        private void pictureBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                dozvil = true;
+                start_point = e.Location;
+            }
+        }
+
+        private void pictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                dozvil = false;
+            }
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open_dialog = new OpenFileDialog();
+            if (open_dialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    full_name_of_image = open_dialog.FileName;
+                    bmp_for_draw = new Bitmap(open_dialog.FileName);
+                    this.pictureBox.SizeMode = PictureBoxSizeMode.AutoSize;
+                    pictureBox.Image = bmp_for_draw;
+                    pictureBox.Invalidate();
+                }
+                catch
+                {
+                    DialogResult result = MessageBox.Show("It's impossible to open selected file");
+                }
+            }
+        }
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            Graphics g = pictureBox.CreateGraphics();
+            g.Clear(SystemColors.Window);
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (pictureBox.Image != null)
+            {
+                string format = full_name_of_image.Substring(full_name_of_image.Length - 5, 5);
+                SaveFileDialog savedialog = new SaveFileDialog();
+                savedialog.Title = "Зберегти як ...";
+                savedialog.OverwritePrompt = true;
+                savedialog.ShowHelp = true;
+                if (savedialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        bmp_for_draw.Save(savedialog.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                    }
+                    catch
+                    {
+                        MessageBox.Show("It's impossible to save image", "FATAL ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+    }
+}
+  
